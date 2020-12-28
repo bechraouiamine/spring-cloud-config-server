a74f09250720e164acbfd9c21a9d2c1cfb7d1370
bechraoui amine <bechraoui.amine@gmail.com>
2020-12-28 23:41:30 +0100

    Initial commit for spring cloud config server online course demo

---
+++ src/test/java/sfg/springframework/springcloudconfig/SpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package sfg.springframework.springcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
